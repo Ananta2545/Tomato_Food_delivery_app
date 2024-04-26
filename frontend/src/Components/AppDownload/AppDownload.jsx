e24703e4813d875acca3341cf74a44847cf12c8c
Ananta2545 <chatterjeeanata091@gmail.com>
2024-04-27 04:12:04 +0530

    added AppDownload components

--- frontend/src/Components/AppDownload/AppDownload.jsx
+++ frontend/src/Components/AppDownload/AppDownload.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import './AppDownload.css'
+import { assets } from '../../assets/assets'
 
 const AppDownload = () => {
   return (
-    <div>AppDownload</div>
+    <div className='appdownload' id='appdownload'>
+        <p>For Better experience download <br /> <span className='Span'>Tomato App</span></p>
+        <div className='appdownload-platforms'>
+            <img src={assets.play_store} alt="" />
+            <img src={assets.app_store} alt="" />
+        </div>
+    </div>
   )
 }
 
