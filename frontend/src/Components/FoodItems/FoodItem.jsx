dfa6e2a1ff0cdddc93ab70f038b346fecf7a4564
Ananta2545 <chatterjeeanata091@gmail.com>
2024-04-27 02:56:35 +0530

    Fixed the cart items update issue

--- frontend/src/Components/FoodItems/FoodItem.jsx
+++ frontend/src/Components/FoodItems/FoodItem.jsx
@@ -1,34 +1,44 @@
-import React, {useContext} from 'react'
-import './FoodItem.css'
-import { assets } from '../../assets/assets'
-import { StoreContext } from '../../Context/StoreContext'
-const FoodItem = ({id,name,price,image,description}) => {
+import React, { useState } from 'react';
+import './FoodItem.css';
+import { assets } from '../../assets/assets';
 
-    
-    const{cartItems,addToCart,removeFromCart}= useContext(StoreContext);
+const FoodItem = ({ id, name, price, image, description }) => {
+  const [quantity, setQuantity] = useState(0);
+
+  const handleAddToCart = () => {
+    setQuantity(quantity + 1);
+  };
+
+  const handleRemoveFromCart = () => {
+    if (quantity > 0) {
+      setQuantity(quantity - 1);
+    }
+  };
+
   return (
     <div className='food-item'>
-        <div className='food-item-img-container'>
-            <img className='food-item-img' src={image} alt="" />
-            {!cartItems[id]? 
-              <img className='add' onClick={()=>addToCart(id)} src={assets.add_icon_white} alt=''/>:
-              <div className='food-item-counter'>
-                    <img onClick={()=>removeFromCart(id)} src={assets.remove_icon_red} alt=""/>
-                    <p>{cartItems[id]}</p>
-                    <img onClick={()=>addToCart(id)} src={assets.add_icon_green} alt="" />
-              </div>
-            }
+      <div className='food-item-img-container'>
+        <img className='food-item-img' src={image} alt='' />
+        {quantity === 0 ? (
+          <img className='add' onClick={handleAddToCart} src={assets.add_icon_white} alt='' />
+        ) : (
+          <div className='food-item-counter'>
+            <img onClick={handleRemoveFromCart} src={assets.remove_icon_red} alt='' />
+            <p>{quantity}</p>
+            <img onClick={handleAddToCart} src={assets.add_icon_green} alt='' />
+          </div>
+        )}
+      </div>
+      <div className='food-item-info'>
+        <div className='food-item-name-rating'>
+          <p>{name}</p>
+          <img src={assets.rating_starts} alt='' />
         </div>
-        <div className="food-item-info">
-            <div className="food-item-name-rating">
-                <p>{name}</p>
-                <img src={assets.rating_starts} alt="" />
-            </div>
-            <p className="food-item-desc">{description}</p>
-            <p className="food-item-price">Rs {price}</p>
-        </div>
+        <p className='food-item-desc'>{description}</p>
+        <p className='food-item-price'>Rs {price}</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default FoodItem
+export default FoodItem;
--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -1,72 +1,54 @@
-import { createContext, useState } from "react";
+import React, { createContext, useState } from "react";
 import { food_list } from "../assets/assets";
-import { useEffect } from "react";
 
-export const StoreContext = createContext(null)
-const StoreContextProvider = (props)=>{
+export const StoreContext = createContext(null);
 
-    const [cartItems,setCartItems]= useState({});
-    const addToCart = (itemId) => {
-        setCartItems((prevCartItems) => {
-            // Create a copy of the previous cartItems state
-            const updatedCartItems = { ...prevCartItems };
-    
-            // Check if the item already exists in the cart
-            if (updatedCartItems[itemId]) {
-                // If item exists, increase its quantity by 1
-                updatedCartItems[itemId] += 1;
-            } else {
-                // If item doesn't exist, add it to the cart with quantity 1
-                updatedCartItems[itemId] = 1;
-            }
-    
-            // Return the updated cartItems object
-            return updatedCartItems;
-        });
-    };
-    
+const StoreContextProvider = (props) => {
+    // const [cartItems, setCartItems] = useState({});
 
+    // const addToCart = (itemId) => {
+    //     setCartItems((prevCartItems) => {
+    //         const updatedCartItems = { ...prevCartItems };
 
-    const removeFromCart = (itemId) => {
-        setCartItems((prevCartItems) => {
-            // Create a copy of the previous cartItems state
-            const updatedCartItems = { ...prevCartItems };
-    
-            // Check if the item exists in the cart
-            if (updatedCartItems[itemId]) {
-                // Decrease the quantity of the item by 1
-                updatedCartItems[itemId] -= 1;
-    
-                // If quantity becomes 0 or less, remove the item from the cart
-                if (updatedCartItems[itemId] <= 0) {
-                    delete updatedCartItems[itemId]; // Remove the item from cartItems
-                }
-            }
-    
-            // Return the updated cartItems object
-            return updatedCartItems;
-        });
-    };
-    
-    
-    useEffect(()=>{
-        console.log(cartItems);
-    },[cartItems])
+    //         if (updatedCartItems[itemId]) {
+    //             updatedCartItems[itemId] += 1;
+    //         } else {
+    //             updatedCartItems[itemId] = 1;
+    //         }
 
+    //         return updatedCartItems;
+    //     });
+    // };
+
+    // const removeFromCart = (itemId) => {
+    //     setCartItems((prevCartItems) => {
+    //         const updatedCartItems = { ...prevCartItems };
+
+    //         if (updatedCartItems[itemId]) {
+    //             updatedCartItems[itemId] -= 1;
+
+    //             if (updatedCartItems[itemId] <= 0) {
+    //                 delete updatedCartItems[itemId];
+    //             }
+    //         }
+
+    //         return updatedCartItems;
+    //     });
+    // };
 
     const contextValue = {
-        food_list,
-        cartItems,
-        setCartItems,
-        addToCart,
-        removeFromCart
+        food_list
+        // cartItems,
+        // setCartItems,
+        // addToCart,
+        // removeFromCart
+    };
 
-    }
     return (
         <StoreContext.Provider value={contextValue}>
             {props.children}
         </StoreContext.Provider>
-    )
-}
+    );
+};
 
 export default StoreContextProvider;
