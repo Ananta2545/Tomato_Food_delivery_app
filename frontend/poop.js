8610449ddbac50d86a85fa22df9a1e8b1c6efb20
Ananta2545 <chatterjeeanata091@gmail.com>
2024-04-28 12:54:38 +0530

    Added login page etc etc

---
+++ frontend/poop.js
@@ -0,0 +1,5 @@
+let srijita = 1;
+let poop = 1;
+if(srijita == poop){
+    console.log("Srijita is poop");
+}
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import { Route, Routes } from 'react-router-dom'
 // import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
 import Home from './pages/Home/Home.jsx'
 import Footer from './Components/Footer/Footer.jsx'
+import LoginPopup from './Components/LoginPopup/LoginPopup.jsx'
 const App = () => {
+  const[showLogin, setShowLogin] = useState(false)
   return (
     <>
+    {showLogin?<LoginPopup setShowLogin={setShowLogin}/>:<></>}
     <div className='app'>
-      <Navbar/>
+      <Navbar setShowLogin={setShowLogin}/>
       <Routes>
       <Route path='/' element={<Home />} />
-      {/* <Route path='/Cart' element = {<Cart/>}/>
-      <Route path='/Order' element = {<PlaceOrder/>}/> */}
+      {/* <Route path='/Cart' element = {<Cart/>}/> */}
+      {/* <Route path='/Order' element = {<PlaceOrder/>}/> */}
       </Routes>
     </div>
     <Footer/>
